d559ff787ecacd287e1a7bcdadeab875d7dd244f
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-08-26 01:25:58 -0500

    feat: :sparkles: release s69 done

---
+++ app/orders/api/route.ts
@@ -0,0 +1,23 @@
+import { prisma } from "@/src/lib/prisma";
+
+export async function GET() {
+  const orders = await prisma.order.findMany({
+    take: 6,
+    where: {
+      doneAt: {
+        not: null
+      }
+    },
+    orderBy: {
+      doneAt: 'desc'
+    },
+    include: {
+      orderProducts: {
+        include: {
+          product: true
+        }
+      }
+    }
+  })
+  return Response.json(orders)
+}   
